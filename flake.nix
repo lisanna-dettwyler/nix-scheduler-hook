7dd88e76d8c71b9d1086617395e05ee23634b146
Lisanna Dettwyler <lisanna.dettwyler@gmail.com>
2025-12-23 18:34:52 -0500

    Add basic slurm tests and github action
    Signed-off-by: Lisanna Dettwyler <lisanna.dettwyler@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "A build hook for Nix that dispatches builds through a job scheduler.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    # nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:lisanna-dettwyler/nixpkgs/add-slurmrestd";
     flake-utils.url = "github:numtide/flake-utils";
     restclient-cpp = {
       url = "github:mrtazz/restclient-cpp";
@@ -10,13 +11,21 @@
     };
   };
 
-  outputs = { self, flake-utils, ... }@inputs: flake-utils.lib.eachDefaultSystem (system: {
-    packages = rec {
-      default = nix-scheduler-hook;
-      nix-scheduler-hook = import ./default.nix {
-        pkgs = import inputs.nixpkgs { inherit system; };
-        restclient-cpp = inputs.restclient-cpp;
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in rec {
+      checks = import ./tests.nix {
+        inherit nixpkgs pkgs;
+        nix-scheduler-hook = packages.default;
       };
-    };
-  });
+      packages = rec {
+        default = nix-scheduler-hook;
+        nix-scheduler-hook = import ./default.nix {
+          inherit pkgs;
+          restclient-cpp = inputs.restclient-cpp;
+        };
+      };
+    }
+  );
 }
