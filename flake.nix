8be88ef82222c99ffb4fe402e79c21b83ac90642
Lisanna Dettwyler <lisanna.dettwyler@gmail.com>
2026-03-11 17:11:36 -0400

    Banish flake-utils 🎉
    Signed-off-by: Lisanna Dettwyler <lisanna.dettwyler@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,27 +3,33 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
     restclient-cpp = {
       url = "github:mrtazz/restclient-cpp";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, ... }@inputs:
     let
-      pkgs = import nixpkgs { inherit system; };
-    in rec {
-      checks = import ./tests.nix {
-        inherit nixpkgs pkgs;
-        nix-scheduler-hook = packages.default;
-      };
-      packages = rec {
+      eachDefaultSystem = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "x86_64-darwin"
+          "aarch64-linux"
+          "aarch64-darwin"
+        ] (system: function (import nixpkgs { inherit system; }) system);
+    in {
+      checks = eachDefaultSystem (pkgs: system:
+        import ./tests.nix {
+          inherit nixpkgs pkgs;
+          nix-scheduler-hook = self.packages.${system}.default;
+        }
+      );
+      packages = eachDefaultSystem (pkgs: system: rec {
         default = nix-scheduler-hook;
         nix-scheduler-hook = pkgs.callPackage ./default.nix {
           restclient-cpp = inputs.restclient-cpp;
         };
-      };
-    }
-  );
+      });
+    };
 }
